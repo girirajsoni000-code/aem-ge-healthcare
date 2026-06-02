87b6bc501524c86884785105156052f00bfc4889
giriraj <giriraj@webanix.in>
2026-06-02 13:05:16 +0530

    change

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -17,6 +17,7 @@ import {
  * @param {Element} from the element to copy attributes from
  * @param {Element} to the element to copy attributes to
  */
+//test
 export function moveAttributes(from, to, attributes) {
   if (!attributes) {
     // eslint-disable-next-line no-param-reassign
